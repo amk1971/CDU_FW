a7016d1ddfe71552b207ae7da3d3d6fccbf3dd06
AliHassanEB <59415795+AliHassanEB@users.noreply.github.com>
2025-02-21 14:53:12 +0500

    Can Config

--- HF_V0.1_CAN/Core/Inc/stm32f1xx_it.h
+++ HF_V0.1_CAN/Core/Inc/stm32f1xx_it.h
@@ -55,6 +55,7 @@ void DebugMon_Handler(void);
 void EXTI0_IRQHandler(void);
 void EXTI1_IRQHandler(void);
 void EXTI2_IRQHandler(void);
+void USB_LP_CAN1_RX0_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void USART1_IRQHandler(void);
